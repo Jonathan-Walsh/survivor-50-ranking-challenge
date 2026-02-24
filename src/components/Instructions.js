308af1d40b159abf076277c9d093c89a7c6091b5
Jonathan Walsh <jlw4699@gmail.com>
2026-02-24 00:09:40 -0800

    Fix typos

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -43,13 +43,13 @@ export function Instructions() {
         ),
         h('h3', null, 'Scoring'),
         h('p', null, 'This game uses a progressive scoring system where you earn *up to* the points in your selected tier.'),
-        h('p', null, 'Example: pick a contestant for Top 12 and get 1 point if they finish Bottom 12, or 2 points if they finish Top 12 or better. For your winner pick, receive 1 point if they place Bottom 12, 2 points if they 7-12, 3 points if the place 4-6, and so on.'),
+        h('p', null, 'Example: pick a contestant for Top 12 and get 1 point if they finish Bottom 12, or 2 points if they finish Top 12 or better. For your winner pick, receive 1 point if they place Bottom 12, 2 points if they 7-12, 3 points if they place 4-6, and so on.'),
         h('p', null, 'Bottom 12 picks are the one exception: score 1 point only if the contestant finishes 13-24 and zero points if they place better.'),
         h('h3', null, 'Playing the Game'),
         h('ol', null,
           h('li', null, 'Drag contestants from the pool into tiers'),
           h('li', null, 'Enter your name and click "Lock In Predictions"'),
-          h('li', null, 'Share your friend code with others and add friends to your own leaderboard. All info is saved in the URL, so bookmark the page once everyone is added to to save your and your friends\' picks.'),
+          h('li', null, 'Share your friend code with others and add friends to your own leaderboard. All info is saved in the URL, so bookmark the page to save your and your friends\' picks.'),
           h('li', null, 'As the season progresses and players are voted out, rankings will update automatically'),
           h('li', null, 'Check the leaderboard to see how your predictions stack up against your friends and family!')
         )
